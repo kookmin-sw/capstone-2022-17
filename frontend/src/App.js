e2d4ed0e0c896b8e93f4cbcc350f1cc69e9ed22d
minjj0905 <minjj0905@kookmin.ac.kr>
2022-03-18 13:35:41 +0900

    chore: initial setting

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Home } from 'pages';
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <h1>Test</h1>
+      <div>teststest</div>
+    </>
+  );
+};
+
+export default App;
