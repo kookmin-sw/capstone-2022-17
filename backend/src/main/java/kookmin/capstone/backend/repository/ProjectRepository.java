23b4111869cffa89c6908bfa59b9c0d1eac64a89
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-03-31 22:10:44 +0900

    feat: Spring Data JPA로 변경

--- backend/src/main/java/kookmin/capstone/backend/repository/ProjectRepository.java
+++ backend/src/main/java/kookmin/capstone/backend/repository/ProjectRepository.java
@@ -2,34 +2,10 @@ package kookmin.capstone.backend.repository;
 
 
 import kookmin.capstone.backend.domain.project.Project;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-@Repository
-@RequiredArgsConstructor
-public class ProjectRepository {
-    
-    private final EntityManager em;
-    
-    public void save(Project project) {
-        em.persist(project);
-    }
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    public Project findOne(Long id) {
-        return em.find(Project.class, id);
-    }
-
-    public List<Project> findAll() {
-        return em.createQuery("select p from Project p", Project.class)
-                .getResultList();
-    }
-
-    public List<Project> findByTitle(String title) {
-        return em.createQuery("select p from Project p where p.title = :title", Project.class)
-                .setParameter("title", title)
-                .getResultList();
-    }
+    List<Project> findByTitle(String title);
 }
--- backend/src/main/java/kookmin/capstone/backend/repository/UserRepository.java
+++ backend/src/main/java/kookmin/capstone/backend/repository/UserRepository.java
@@ -3,21 +3,10 @@ package kookmin.capstone.backend.repository;
 import kookmin.capstone.backend.domain.User;
 import kookmin.capstone.backend.domain.project.Project;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    private final EntityManager em;
-
-    public void save(User user) {
-        if (user.getId() == null) {
-            em.persist(user);
-        } else {
-            em.merge(user);
-        }
-    }
+public interface UserRepository extends JpaRepository<User, Long> {
 }
