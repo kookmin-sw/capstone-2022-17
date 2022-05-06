7415dd1a04d4ce4fb65555a28f25ccae5d25bb5d
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-05-06 17:05:37 +0900

    fix: arrow func으로 수정

--- frontend/src/components/Tag/Tag.js
+++ frontend/src/components/Tag/Tag.js
@@ -2,18 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagDiv = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.text};
-    color: #707070;
-    background-color: #E9EAEB;
-    border-radius: 0.2rem;
-    padding: 0.3rem;
+  font-size: ${({ theme }) => theme.fontSize.text};
+  color: #707070;
+  background-color: #e9eaeb;
+  border-radius: 0.2rem;
+  padding: 0.3rem;
 `;
 
-function Tag(props) {
-    //태그 props로 받아서 나타냄.
-    return (
-        <TagDiv> {props.tagName} </TagDiv>
-    );
-}
+const Tag = ({ children }) => {
+  return <TagDiv> {children} </TagDiv>;
+};
 
-export default Tag; 
+export default Tag;
