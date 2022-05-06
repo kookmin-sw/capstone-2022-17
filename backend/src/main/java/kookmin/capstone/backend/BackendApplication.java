47051b5fc0c0cbecd7551f23fe54f7bf54ac879e
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-05-06 19:57:23 +0900

    fix: yml 의존성 임시 수정

--- backend/src/main/java/kookmin/capstone/backend/BackendApplication.java
+++ backend/src/main/java/kookmin/capstone/backend/BackendApplication.java
@@ -16,8 +16,8 @@ import java.awt.print.Book;
 public class BackendApplication {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:application-aws.yml";
+            + "classpath:application.yml";
+//            + "classpath:application-aws.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(BackendApplication.class)
