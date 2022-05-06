7a081a38d5262d9f4dfaefc9588d55ba7d6430c7
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-05-06 17:00:06 +0900

    fix: arrow func으로 수정, collectedCount를 nowCount로 수정

--- frontend/src/components/Card/CardHeadCount.js
+++ frontend/src/components/Card/CardHeadCount.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const HeadCount = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.text};
-    color: ${({ theme }) => theme.color.primary};
-    display: flex;
+  font-size: ${({ theme }) => theme.fontSize.text};
+  color: ${({ theme }) => theme.color.primary};
+  display: flex;
 `;
 
 const HeadCountImg = styled.img`
-    width: 100%;
+  width: 100%;
 `;
 
-function CardHeadCount(props) {
-    //현재 모집인원수와 총인원수 props로 받아서 나타냄.
-    return (
-        <HeadCount> 
-            <HeadCountImg src="card/headCountImg.png"></HeadCountImg>
-            {props.collectedCount}/{props.total} 
-        </HeadCount>
-    );
-}
-
-export default CardHeadCount; 
+// 현재모집인원/총인원수 보여줌
+const CardHeadCount = ({ nowCount, total }) => {
+  return (
+    <HeadCount>
+      <HeadCountImg src="card/headCountImg.png" />
+      {nowCount}/{total}
+    </HeadCount>
+  );
+};
+export default CardHeadCount;
