b9fbb17d42006b3ade102fae005c2b5219ce1fcb
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-05-06 15:01:10 +0900

    fix: arrow func로 수정

--- frontend/src/components/Card/CardPeriod.js
+++ frontend/src/components/Card/CardPeriod.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
 const ProjectPeriod = styled.div`
-    font-size: ${({ theme }) => theme.fontSize.text};
-    color: #A0A0A0;
+  font-size: ${({ theme }) => theme.fontSize.text};
+  color: #a0a0a0;
 `;
 
-function CardPeriod(props) {
-    //모집날짜 props로 받아서 나타냄.
-    return (
-        <ProjectPeriod> {props.startDate} ~ {props.endDate} </ProjectPeriod>
-    );
-}
+// 모집날짜
+const CardPeriod = ({ children }) => {
+  return <ProjectPeriod> {children} </ProjectPeriod>;
+};
 
-export default CardPeriod; 
+export default CardPeriod;
