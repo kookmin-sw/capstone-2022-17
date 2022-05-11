96487de214f2c7866b76f49e37f82fdd0185ecb1
minjj0905 <minjj0905@kookmin.ac.kr>
2022-05-11 20:16:04 +0900

    style: change font

--- frontend/src/components/common/Btn.jsx
+++ frontend/src/components/common/Btn.jsx
@@ -6,8 +6,7 @@ import COLOR from 'constant/color';
 export const PrimaryBtn = styled(Button)`
   background: linear-gradient(to right, ${COLOR.PRIMARY}, ${COLOR.SUB}) !important;
   color: white !important;
-  font-family: 'PR-Medium' !important;
-  font-weight: 100 !important;
+  font-family: 'PR-Light' !important;
   height: 3rem;
   font-size: 18px !important;
 
