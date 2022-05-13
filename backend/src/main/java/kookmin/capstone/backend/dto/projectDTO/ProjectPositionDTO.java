cc4e21170e58201442a7f6050d92104c4406f735
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-05-14 00:17:12 +0900

    feat: 프로젝트 포지션 현황 조회 API 작성

--- backend/src/main/java/kookmin/capstone/backend/dto/projectDTO/ProjectPositionDTO.java
+++ backend/src/main/java/kookmin/capstone/backend/dto/projectDTO/ProjectPositionDTO.java
@@ -1,5 +1,6 @@
 package kookmin.capstone.backend.dto.projectDTO;
 
+import kookmin.capstone.backend.domain.project.ProjectPosition;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,4 +14,13 @@ public class ProjectPositionDTO {
     private int total;
     private int currentCnt;
     private String positionName;
+
+    public static ProjectPositionDTO entityToDto(ProjectPosition projectPosition) {
+        ProjectPositionDTO projectPositionDTO = ProjectPositionDTO.builder().
+                positionName(projectPosition.getPosition().getPositionName()).
+                currentCnt(projectPosition.getCurrentCnt()).
+                total(projectPosition.getTotal()).
+                build();
+        return projectPositionDTO;
+    }
 }
--- backend/src/main/java/kookmin/capstone/backend/response/ResponseMessage.java
+++ backend/src/main/java/kookmin/capstone/backend/response/ResponseMessage.java
@@ -8,4 +8,5 @@ public class ResponseMessage {
     public static final String DUPLICATED_NICKNAME = "중복 닉네임 입니다.";
     public static final String NICKNAME_CHECK = "사용 가능한 닉네임 입니다.";
     public static final String USER_UPDATE_SUCCESS = "유저 정보 업데이트 성공";
+    public static final String POSITION_GET_SUCCESS = "포지션 현황 정보 가져오기 성공";
 }
