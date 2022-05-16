0b8243c8e67ab024b0005b9685a66f9dac8ac686
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-05-16 20:23:11 +0900

    feat: 테크 스택 리스트 보내기 API 추가

--- backend/src/main/java/kookmin/capstone/backend/repository/TechStackRepository.java
+++ backend/src/main/java/kookmin/capstone/backend/repository/TechStackRepository.java
@@ -1,10 +1,18 @@
 package kookmin.capstone.backend.repository;
 
 import kookmin.capstone.backend.domain.TechStack;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TechStackRepository extends JpaRepository<TechStack, Long> {
 
-    boolean existsByStack(String stack);
+    @Query("select tech from TechStack tech where tech.stack like concat(:name,'%')")
+    List<TechStack> findByNameStart(@Param("name") String name, Pageable pageable);
+
+    @Query("select tech from TechStack tech where tech.stack like concat('% ', :name,'%')")
+    List<TechStack> findByNameContain(@Param("name") String name, Pageable pageable);
 }
--- backend/src/main/java/kookmin/capstone/backend/service/TechStackService.java
+++ backend/src/main/java/kookmin/capstone/backend/service/TechStackService.java
@@ -1,13 +1,18 @@
 package kookmin.capstone.backend.service;
 
 import kookmin.capstone.backend.domain.TechStack;
-import kookmin.capstone.backend.dto.TechDTO;
+import kookmin.capstone.backend.dto.TechStackDTO;
 import kookmin.capstone.backend.repository.TechStackRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.digester.ArrayStack;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -16,18 +21,22 @@ public class TechStackService {
 
     private final TechStackRepository techStackRepository;
 
-    @Transactional
-    public void registStack(TechDTO techDTO) {
-        List<String> techNames = techDTO.getTechNames();
+    public List<TechStackDTO> getTechStack(String name) {
+        Pageable limitTen = PageRequest.of(0, 10);
+        Pageable limitTen2 = PageRequest.of(0, 10);
 
-        for (String techName : techNames) {
-            if (!techStackRepository.existsByStack(techName)) {
-                TechStack techStack = TechStack.builder().
-                        stack(techName).
-                        build();
-                techStackRepository.save(techStack);
-            }
-        }
+        List<TechStackDTO> stackDTOList = new ArrayStack<>();
+        techStackRepository.findByNameStart(name, limitTen).stream().forEach(techStack ->
+        stackDTOList.add(TechStackDTO.entityToDto(techStack))
+                );
+
+        techStackRepository.findByNameContain(name, limitTen2).stream().forEach(techStack ->
+                stackDTOList.add(TechStackDTO.entityToDto(techStack))
+        );
+
+        List<TechStackDTO> distinctList = stackDTOList.stream().distinct().collect(Collectors.toList());
+//        Collections.sort(distinctList);
+        return distinctList;
     }
 
 }
