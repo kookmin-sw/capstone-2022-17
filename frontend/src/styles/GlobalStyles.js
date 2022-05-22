21d5482a0232ec71eced39219177b444f12a7c5a
kyejin0412 <kyejin0412@kookmin.ac.kr>
2022-05-23 02:39:42 +0900

    feat: 페이지네이션 구현중

--- frontend/src/styles/GlobalStyles.js
+++ frontend/src/styles/GlobalStyles.js
@@ -1,5 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import COLOR from 'constant/color';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
@@ -35,6 +36,16 @@ const GlobalStyles = createGlobalStyle`
   input[type="number"]::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
+}
+
+.ui.secondary.menu .active.item {
+  font-family: 'Pr-Regular';
+  background: none;
+  color: ${COLOR.PRIMARY};
+}
+
+.ui.pagination.menu .item {
+  min-width: 0;
 }
   
 `;
