3d9af816b6e6a6b88da9860d9a6695f49efac133
minjj0905 <minjj0905@kookmin.ac.kr>
2022-05-24 04:13:05 +0900

    feat: account routing 삭제

--- frontend/src/pages/Account/Account.jsx
+++ frontend/src/pages/Account/Account.jsx
@@ -1,45 +1,46 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+// import React, { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
 import TermsModal from 'components/Terms/TermsModal';
 import ServiceModal from 'components/Terms/ServiceModal';
 import * as Container from 'components/common/Containers';
-import { DESTROY_USER_REQUEST } from 'reducers/authentication';
+// import { DESTROY_USER_REQUEST } from 'reducers/authentication';
 import * as Styled from './AccountStyeld';
 
 const Account = () => {
-  const dispatch = useDispatch();
-  const { destroyUserDone } = useSelector((state) => state.authentication);
+  // const dispatch = useDispatch();
+  // const { destroyUserDone } = useSelector((state) => state.authentication);
 
-  const handleWithdraw = () => {
-    if (window.confirm('정말 탈퇴하시겠습니까? 😥')) {
-      dispatch({
-        type: DESTROY_USER_REQUEST,
-      });
-      alert('탈퇴되었습니다.');
-    } else {
-      alert('취소되었습니다 😊');
-    }
-  };
+  // const handleWithdraw = () => {
+  //   if (window.confirm('정말 탈퇴하시겠습니까? 😥')) {
+  //     dispatch({
+  //       type: DESTROY_USER_REQUEST,
+  //     });
+  //     alert('탈퇴되었습니다.');
+  //   } else {
+  //     alert('취소되었습니다 😊');
+  //   }
+  // };
 
-  useEffect(() => {
-    if (destroyUserDone) {
-      window.location.replace('/');
-    }
-  });
+  // useEffect(() => {
+  //   if (destroyUserDone) {
+  //     window.location.replace('/');
+  //   }
+  // });
 
   return (
     <Container.AlignCenterContainer>
       <Styled.SettingContainer>
         <Styled.Line />
 
-        <Styled.Container>
-          <Styled.LightText>
+        {/* <Styled.Container> */}
+        {/* <Styled.LightText>
             탈퇴 시 작성한 글 및 댓글이 삭제되며 복구되지 않습니다.
-          </Styled.LightText>
-          <Styled.Btn style={{ minWidth: '4.8rem', marginLeft: '1rem' }} onClick={handleWithdraw}>
+          </Styled.LightText> */}
+        {/* <Styled.Btn style={{ minWidth: '4.8rem', marginLeft: '1rem' }} onClick={handleWithdraw}>
             회원 탈퇴
-          </Styled.Btn>
-        </Styled.Container>
+          </Styled.Btn> */}
+        {/* </Styled.Container> */}
         <Styled.Title style={{ width: '100%' }}>
           <ServiceModal />
           <div style={{ width: '1rem' }} />
