11824fac33320354411eb4695f58c3afba7fe548
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-05-23 13:25:40 +0900

    feat: 지원 유저 조회시 테크스택도 같이 전달

--- backend/src/main/java/kookmin/capstone/backend/dto/userDTO/UserResDTO.java
+++ backend/src/main/java/kookmin/capstone/backend/dto/userDTO/UserResDTO.java
@@ -7,6 +7,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data @Builder
 @NoArgsConstructor
 public class UserResDTO {
@@ -22,8 +26,12 @@ public class UserResDTO {
 
     private String position;
 
+    private List<String> userTechStack;
+
     @QueryProjection
-    public UserResDTO(Long userId, String nickname, String avatar, Float rating, String instaId, String blog, String github, String introduce, String position) {
+    public UserResDTO(Long userId, String nickname, String avatar,
+                      Float rating, String instaId, String blog, String github,
+                      String introduce, String position, List<String> userTechStack) {
         this.userId = userId;
         this.nickname = nickname;
         this.avatar = avatar;
@@ -33,6 +41,7 @@ public class UserResDTO {
         this.github = github;
         this.introduce = introduce;
         this.position = position;
+        this.userTechStack = userTechStack;
     }
     //TODO
     public static UserResDTO entityToDto(User user) {
@@ -45,6 +54,8 @@ public class UserResDTO {
                 blog(user.getBlog()).
                 github(user.getGithub()).
                 introduce(user.getIntroduce()).
+                userTechStack(user.getTechStack().stream().map(e -> e.getStack())
+                        .collect(Collectors.toCollection(ArrayList::new))).
                 build();
     }
 }
